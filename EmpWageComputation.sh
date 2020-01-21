c186c00713bf5ac29cdb3236d293b4fe40741779
PalakPartani <partanipalak16@gmail.com>
2020-01-21 16:56:17 +0530

    MODIFIED:Case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -8,17 +8,23 @@ readonly FULL_DAY_HOUR=8
 readonly PART_TIME_HOUR=4
 
 check=$((RANDOM%3))
-if(($check==1))
-then
-	echo "Emp Present"
-	dailyWage==$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-	echo "Daily Employee Wage: " $dailyWage
-elif(($check==2))
-	then
-	echo "Employee present is part time"
-	parttime_DailyWage==$(($WAGE_PER_HOUR*$PART_TIME_HOUR))	
-	echo "Part time employee wage : " $parttime_DailyWage
-	
-else
-	echo "Emp Absent"
-fi
+isParttime=2
+isFulltime=1
+
+case $check in
+$isParttime )
+   echo "Employee present is part time"
+   parttime_DailyWage==$(($WAGE_PER_HOUR*$PART_TIME_HOUR))  
+   echo "Part time employee wage : " $parttime_DailyWage
+;;
+
+$isFulltime )
+   echo "Emp Present"
+   dailyWage==$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
+   echo "Daily Employee Wage: " $dailyWage
+;;
+* )
+ echo "Emp Absent"
+esac
+
+
