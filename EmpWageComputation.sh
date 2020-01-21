74d47a3bba3587b5334d4f444156ddaba0c3e71d
PalakPartani <partanipalak16@gmail.com>
2020-01-21 16:11:08 +0530

    ADDED:Parttime and wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,14 +5,20 @@ echo "Welcome to EmpWage computation"
 
 readonly WAGE_PER_HOUR=20
 readonly FULL_DAY_HOUR=8
+readonly PART_TIME_HOUR=4
 
-isPresent=1
-checkPresent=$((RANDOM%2))
-if(($checkPresent==$isPresent))
+check=$((RANDOM%3))
+if(($check==1))
 then
 	echo "Emp Present"
 	dailyWage==$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
 	echo "Daily Employee Wage: " $dailyWage
+elif(($check==2))
+	then
+	echo "Employee present is part time"
+	parttime_DailyWage==$(($WAGE_PER_HOUR*$PART_TIME_HOUR))	
+	echo "Part time employee wage : " $parttime_DailyWage
+	
 else
 	echo "Emp Absent"
 fi
